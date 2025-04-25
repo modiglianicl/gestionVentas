990c56d8b981e19ac2eefc439b07a868ce33868e
DUOCACAD\vina <kek@jej.com>
2025-04-24 21:22:59 -0400

    Nuevo POST

--- src/main/java/cl/perfulandia/gestionVentas/controller/ventasController.java
+++ src/main/java/cl/perfulandia/gestionVentas/controller/ventasController.java
@@ -1,18 +1,21 @@
 package cl.perfulandia.gestionVentas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/ventas")
+@RequestMapping("/api")
 public class ventasController {
+    HashMap<Integer, String> ventas = new HashMap<>();
+    HashMap<Integer,String> ventasRealizadas = new HashMap<>();
 
-    @GetMapping("/")
+    @GetMapping("/ventas")
     public HashMap mostrarVentas(){
-        HashMap<Integer, String> ventas = new HashMap<>();
+
         int contador = 1;
         for (int i = 0; i < 5000; i++) {
             ventas.put(contador, "Venta " + contador);
@@ -20,6 +23,18 @@ public class ventasController {
         }
 
         return ventas;
+    }
+
+    @PostMapping("/ventas")
+    public HashMap crearVenta(){
+
+        int contador = 1;
+        for (int i = 0; i < 5000; i++) {
+            ventasRealizadas.put(1,"Venta " + contador + "realizada por POST!");
+            contador++;
+        }
+
+        return ventasRealizadas;
     }
 
 }
